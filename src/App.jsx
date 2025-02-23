ec46439b1f29f03f0717ffa623200ed6784c0186
Anurag <anuragsonkar053@gmail.com>
2025-02-23 13:44:20 +0530

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PostProperty from './pages/PostProperty'
+
+function App() {
+  return (
+    <div><PostProperty /></div>
+  )
+}
+
+export default App
---
+++ src/utils/base_url.js
@@ -0,0 +1,5 @@
+// Propertify
+export const base_url = 'https://propertify.onrender.com'
+
+// Local
+// export const base_url = 'http://localhost:5053'
